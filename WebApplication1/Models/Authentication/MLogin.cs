67608ab76511590af7336d7893c798d3e73ee489
Кочанжи Ира <irinacoceanji2003@gmail.com>
2024-03-29 14:37:56 +0200

    test1

--- WebApplication1/Models/Authentication/MLogin.cs
+++ WebApplication1/Models/Authentication/MLogin.cs
@@ -12,6 +12,7 @@ namespace WebApplication1.Models.Authentication
         [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
 
+
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
     }
