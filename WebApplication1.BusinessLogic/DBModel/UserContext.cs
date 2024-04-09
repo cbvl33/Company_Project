6f407cca1ff9621e02c5680861e1361e44938c67
Валерия Чебан <cebanvaleria402@gmail.com>
2024-04-09 09:05:56 +0300

    bd

---
+++ WebApplication1.BusinessLogic/DBModel/UserContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using WebApplication1.Domain.Entities.User;
+
+
+namespace WebApplication1.BusinessLogic.DBModel
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base (nameOrConnectionString: "name=WebApplication1") 
+        { 
+
+        }
+
+        public virtual DbSet<UDbTable> Users { get; set; }
+    }
+}
---
+++ WebApplication1.Domain/Entities/User/UDbTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Domain.Enums.Roles;
+
+namespace WebApplication1.Domain.Entities.User
+{
+    public class UDbTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 5)]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 8)]
+        public string Password { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime LastLogin { get; set; }
+        public string LastIp { get; set; }
+        [Required]
+        public URole Level { get; set; }
+    }
+}
