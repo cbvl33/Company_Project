039e43103edc6abfd66831092d35c2ba73b63c28
Валерия Чебан <cebanvaleria402@gmail.com>
2024-04-23 18:07:19 +0300

    новые папки

---
+++ WebApplication1.BusinessLogic/DBModel/SessionContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Domain.Entities.User.DbTable;
+
+namespace WebApplication1.BusinessLogic.DBModel
+{
+    public class SessionContext : DbContext
+    {
+        public SessionContext() : base("name=WebApplication1") { }
+
+        public virtual DbSet<UDbSession> Sessions { get; set; }
+    }
+}
--- WebApplication1.BusinessLogic/DBModel/UserContext.cs
+++ WebApplication1.BusinessLogic/DBModel/UserContext.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebApplication1.Domain.Entities.User;
+using WebApplication1.Domain.Entities.User.DbTable;
 
 namespace WebApplication1.BusinessLogic.DBModel
 {
@@ -12,5 +13,12 @@ namespace WebApplication1.BusinessLogic.DBModel
     {
         public UserContext() : base(nameOrConnectionString:"name=WebApplication1"){ }
         public virtual DbSet<UserDTO> Users { get; set; }
+        public virtual DbSet<UDbAddInfo> AddInfos { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserDTO>()
+                .HasOptional(l => l.AddInfo)
+                .WithRequired(l => l.User);
+        }
     }
 }
